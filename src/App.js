8feb899f29b2e4267ff726d6249596846c2778f6
riteshm9653 <mritesh9653@gmail.com>
2024-11-16 15:34:12 +0530

    Netfilx-clone

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl  font-semibold text-red-600">
+      Hello ji or namaster every one or bolo
+    </div>
+  );
+}
+
+export default App;
